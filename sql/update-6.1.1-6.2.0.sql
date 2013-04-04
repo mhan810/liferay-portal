2b270912409bc961fc29cf5c316f7a713b9a9b8f
David Gonzalez <david.gonzalez@liferay.com>
2013-04-03 20:05:49 -0500

    LPS-34165 Update sql file

--- sql/update-6.1.1-6.2.0.sql
+++ sql/update-6.1.1-6.2.0.sql
@@ -399,6 +399,8 @@ alter table MBThreadFlag add companyId LONG;
 alter table MBThreadFlag add userName VARCHAR(75) null;
 alter table MBThreadFlag add createDate DATE null;
 
+alter table Organization_ add uuid_ VARCHAR(75) null;
+
 drop table OrgGroupPermission;
 
 alter table PasswordPolicy add regex VARCHAR(75) null;
@@ -435,6 +437,8 @@ drop index IX_C94C7708 on ResourcePermission;
 drop index IX_8D83D0CE on ResourcePermission;
 drop index IX_4A1F4402 on ResourcePermission;
 drop index IX_8DB864A9 on ResourcePermission;
+
+alter table Role_ add uuid_ VARCHAR(75) null;
 
 drop table Roles_Permissions;
 
@@ -485,6 +489,8 @@ alter table User_ add ldapServerId LONG;
 COMMIT_TRANSACTION;
 
 update User_ set ldapServerId = -1;
+
+alter table UserGroup add uuid_ VARCHAR(75) null;
 
 drop table Users_Permissions;
 
