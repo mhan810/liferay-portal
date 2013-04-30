1f1543716a8b92e9b8ca7c25ed846e0f1fe88ed1
David Gonzalez <david.gonzalez@liferay.com>
2013-04-30 11:39:19 -0500

    LPS-34381 RolesAdmin and UsersAdmin Portlet Data Handlers

--- portal-impl/src/com/liferay/portlet/rolesadmin/lar/RolesAdminPortletDataHandler.java
+++ portal-impl/src/com/liferay/portlet/rolesadmin/lar/RolesAdminPortletDataHandler.java
@@ -14,13 +14,102 @@
 
 package com.liferay.portlet.rolesadmin.lar;
 
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.lar.BasePortletDataHandler;
+import com.liferay.portal.kernel.lar.PortletDataContext;
+import com.liferay.portal.kernel.lar.StagedModelDataHandlerUtil;
+import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portal.model.Role;
+import com.liferay.portal.service.persistence.RoleActionableDynamicQuery;
+
+import java.util.List;
+
+import javax.portlet.PortletPreferences;
 
 /**
  * @author Michael C. Han
+ * @author David Mendez Gonzalez
  */
 public class RolesAdminPortletDataHandler extends BasePortletDataHandler {
 
 	public static final String NAMESPACE = "roles_admin";
+
+	@Override
+	protected PortletPreferences doDeleteData(
+			PortletDataContext portletDataContext, String portletId,
+			PortletPreferences portletPreferences)
+		throws Exception {
+
+		throw new UnsupportedOperationException(
+			"Cannot delete portlet data for roles");
+	}
+
+	@Override
+	protected String doExportData(
+			final PortletDataContext portletDataContext, String portletId,
+			PortletPreferences portletPreferences)
+		throws Exception {
+
+		portletDataContext.addPermissions(
+			"com.liferay.portlet.rolesadmin",
+			portletDataContext.getScopeGroupId());
+
+		Element rootElement = addExportDataRootElement(portletDataContext);
+
+		rootElement.addAttribute(
+			"group-id", String.valueOf(portletDataContext.getScopeGroupId()));
+
+		ActionableDynamicQuery roleActionableDynamicQuery =
+			new RoleActionableDynamicQuery() {
+
+			@Override
+			protected void addCriteria(DynamicQuery dynamicQuery) {
+				portletDataContext.addDateRangeCriteria(
+					dynamicQuery, "modifiedDate");
+			}
+
+			@Override
+			protected void performAction(Object object) throws PortalException {
+				Role role = (Role)object;
+
+				StagedModelDataHandlerUtil.exportStagedModel(
+					portletDataContext, role);
+			}
+
+		};
+
+		roleActionableDynamicQuery.setGroupId(
+			portletDataContext.getScopeGroupId());
+
+		roleActionableDynamicQuery.performActions();
+
+		return getExportDataRootElementString(rootElement);
+	}
+
+	@Override
+	protected PortletPreferences doImportData(
+			PortletDataContext portletDataContext, String portletId,
+			PortletPreferences portletPreferences, String data)
+		throws Exception {
+
+		portletDataContext.importPermissions(
+			"com.liferay.portlet.rolesadmin",
+			portletDataContext.getSourceGroupId(),
+			portletDataContext.getScopeGroupId());
+
+		Element rolesElement = portletDataContext.getImportDataGroupElement(
+			Role.class);
+
+		List<Element> roleElements = rolesElement.elements();
+
+		for (Element roleElement : roleElements) {
+			StagedModelDataHandlerUtil.importStagedModel(
+				portletDataContext, roleElement);
+		}
+
+		return null;
+	}
 
 }
