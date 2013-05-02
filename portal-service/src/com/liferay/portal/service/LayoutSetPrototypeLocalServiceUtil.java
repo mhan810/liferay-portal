9e7f22e4ffe1cde67223e618e13a3e7391a3f5f9
driesco <daniela.riesco@liferay.com>
2013-05-02 11:02:26 -0500

    LPS-34381 Regenerate

--- portal-service/src/com/liferay/portal/service/LayoutSetPrototypeLocalServiceUtil.java
+++ portal-service/src/com/liferay/portal/service/LayoutSetPrototypeLocalServiceUtil.java
@@ -267,6 +267,12 @@ public class LayoutSetPrototypeLocalServiceUtil {
 			description, active, layoutsUpdateable, serviceContext);
 	}
 
+	public static void deleteLayoutSetPrototypes(long companyId)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		getService().deleteLayoutSetPrototypes(companyId);
+	}
+
 	public static com.liferay.portal.model.LayoutSetPrototype fetchLayoutSetPrototypeByUuidAndCompanyId(
 		java.lang.String uuid, long companyId)
 		throws com.liferay.portal.kernel.exception.SystemException {
