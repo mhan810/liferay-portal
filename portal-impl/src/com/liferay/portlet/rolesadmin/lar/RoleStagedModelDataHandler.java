7f68fd9ba161b568d5735bb80a699e2d8600c73b
Michael Han <michael.han@liferay.com>
2013-05-08 11:40:26 -0500

    LPS-17439 add role export/import

--- portal-impl/src/com/liferay/portlet/rolesadmin/lar/RoleStagedModelDataHandler.java
+++ portal-impl/src/com/liferay/portlet/rolesadmin/lar/RoleStagedModelDataHandler.java
@@ -14,16 +14,35 @@
 
 package com.liferay.portlet.rolesadmin.lar;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.lar.BaseStagedModelDataHandler;
 import com.liferay.portal.kernel.lar.ExportImportPathUtil;
 import com.liferay.portal.kernel.lar.PortletDataContext;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portal.model.GroupConstants;
+import com.liferay.portal.model.Permission;
+import com.liferay.portal.model.ResourceConstants;
+import com.liferay.portal.model.ResourcePermission;
+import com.liferay.portal.model.ResourceTypePermission;
 import com.liferay.portal.model.Role;
+import com.liferay.portal.security.permission.PermissionConversionFilter;
+import com.liferay.portal.security.permission.PermissionConverter;
+import com.liferay.portal.security.permission.PermissionConverterUtil;
+import com.liferay.portal.service.ResourceBlockLocalServiceUtil;
+import com.liferay.portal.service.ResourcePermissionLocalServiceUtil;
+import com.liferay.portal.service.ResourcePermissionServiceUtil;
+import com.liferay.portal.service.ResourceTypePermissionLocalServiceUtil;
 import com.liferay.portal.service.RoleLocalServiceUtil;
 import com.liferay.portal.service.ServiceContext;
 
+import java.util.List;
+
 /**
  * @author David Mendez Gonzalez
+ * @author Michael C. Han
  */
 public class RoleStagedModelDataHandler
 	extends BaseStagedModelDataHandler<Role> {
@@ -33,6 +52,53 @@ public class RoleStagedModelDataHandler
 	@Override
 	public String[] getClassNames() {
 		return CLASS_NAMES;
+	}
+
+	protected void clearPermissions(
+			PortletDataContext portletDataContext, Role importedRole)
+		throws PortalException, SystemException {
+
+		List<ResourceTypePermission> resourceTypePermissions =
+			ResourceTypePermissionLocalServiceUtil.
+				getRoleResourceTypePermissions(importedRole.getRoleId());
+
+		for (ResourceTypePermission resourceTypePermission :
+				resourceTypePermissions) {
+
+			ResourceTypePermissionLocalServiceUtil.deleteResourceTypePermission(
+				resourceTypePermission);
+		}
+
+		List<ResourcePermission> resourcePermissions =
+			ResourcePermissionLocalServiceUtil.getRoleResourcePermissions(
+				importedRole.getRoleId(), new int[] {
+					ResourceConstants.SCOPE_COMPANY,
+					ResourceConstants.SCOPE_GROUP_TEMPLATE},
+				0, -1);
+
+		for (ResourcePermission resourcePermission : resourcePermissions) {
+			ResourcePermissionLocalServiceUtil.deleteResourcePermission(
+				resourcePermission);
+		}
+
+		List<ResourcePermission> groupResourcePermissions =
+			ResourcePermissionLocalServiceUtil.getRoleResourcePermissions(
+				importedRole.getRoleId(), new int[] {
+					ResourceConstants.SCOPE_GROUP},
+				0, -1);
+
+		for (ResourcePermission groupResourcePermission :
+				groupResourcePermissions) {
+
+			long groupId = Long.parseLong(groupResourcePermission.getPrimKey());
+
+			if ((groupId == portletDataContext.getCompanyGroupId()) ||
+				(groupId == portletDataContext.getUserPersonalSiteGroupId())) {
+
+				ResourcePermissionLocalServiceUtil.deleteResourcePermission(
+					groupResourcePermission);
+			}
+		}
 	}
 
 	@Override
@@ -45,6 +111,19 @@ public class RoleStagedModelDataHandler
 		portletDataContext.addClassedModel(
 			roleElement, ExportImportPathUtil.getModelPath(role), role,
 			RolesAdminPortletDataHandler.NAMESPACE);
+
+		PermissionConverter permissionConverter =
+			PermissionConverterUtil.getPermissionConverter();
+
+		List<Permission> permissions = permissionConverter.convertPermissions(
+			role, _permissionConversionFilter);
+
+		String xml = portletDataContext.toXML(permissions);
+
+		String permissionsPath = ExportImportPathUtil.getModelPath(
+			role, "permissions.xml");
+
+		portletDataContext.addZipEntry(permissionsPath, xml);
 	}
 
 	@Override
@@ -79,11 +158,129 @@ public class RoleStagedModelDataHandler
 			importedRole = RoleLocalServiceUtil.updateRole(
 				existingRole.getRoleId(), role.getName(), role.getTitleMap(),
 				role.getDescriptionMap(), role.getSubtype(), serviceContext);
+
+			clearPermissions(portletDataContext, importedRole);
 		}
 
 		portletDataContext.importClassedModel(
 			role, importedRole, RolesAdminPortletDataHandler.NAMESPACE);
 
+		String permissionsPath = ExportImportPathUtil.getModelPath(
+			role, "permissions.xml");
+
+		List<Permission> permissions =
+			(List<Permission>)portletDataContext.getZipEntryAsObject(
+				permissionsPath);
+
+		for (Permission permission : permissions) {
+			if (ResourceBlockLocalServiceUtil.isSupported(
+					permission.getName())) {
+
+				importResourceBlock(
+					portletDataContext, importedRole, permission);
+			}
+			else {
+				importResourcePermissions(
+					portletDataContext, importedRole, permission);
+			}
+		}
 	}
+
+	protected void importResourceBlock(
+			PortletDataContext portletDataContext, Role importedRole,
+			Permission permission)
+		throws PortalException, SystemException {
+
+		String actionId = permission.getActionId();
+		long sourceGroupId = Long.parseLong(permission.getPrimKey());
+		int scope = permission.getScope();
+		String name = permission.getName();
+
+		if (scope == ResourceConstants.SCOPE_COMPANY) {
+			ResourceBlockLocalServiceUtil.addCompanyScopePermission(
+				portletDataContext.getCompanyId(), name,
+				importedRole.getRoleId(), actionId);
+		}
+		else if (scope == ResourceConstants.SCOPE_GROUP_TEMPLATE) {
+			ResourceBlockLocalServiceUtil.addGroupScopePermission(
+				portletDataContext.getCompanyId(),
+				GroupConstants.DEFAULT_PARENT_GROUP_ID, name,
+				importedRole.getRoleId(), actionId);
+		}
+		else if (scope == ResourceConstants.SCOPE_GROUP) {
+			long groupId = portletDataContext.getCompanyGroupId();
+
+			if (sourceGroupId ==
+					portletDataContext.getSourceUserPersonalSiteGroupId()) {
+
+				groupId = portletDataContext.getUserPersonalSiteGroupId();
+			}
+
+			ResourceBlockLocalServiceUtil.addGroupScopePermission(
+				portletDataContext.getCompanyId(), groupId, name,
+				importedRole.getRoleId(), actionId);
+		}
+		else {
+			if (_log.isDebugEnabled()) {
+				_log.debug(
+					"Individual scoped permission import not supported: " +
+						permission.toString());
+			}
+		}
+	}
+
+	protected void importResourcePermissions(
+			PortletDataContext portletDataContext, Role importedRole,
+			Permission permission)
+		throws PortalException, SystemException {
+
+		String actionId = permission.getActionId();
+		long sourceGroupId = Long.parseLong(permission.getPrimKey());
+		int scope = permission.getScope();
+		String name = permission.getName();
+
+		if (scope == ResourceConstants.SCOPE_COMPANY) {
+			ResourcePermissionServiceUtil.addResourcePermission(
+				portletDataContext.getCompanyGroupId(),
+				portletDataContext.getCompanyId(), name, scope,
+				String.valueOf(portletDataContext.getCompanyGroupId()),
+				importedRole.getRoleId(), actionId);
+		}
+		else if (scope == ResourceConstants.SCOPE_GROUP_TEMPLATE) {
+			ResourcePermissionServiceUtil.addResourcePermission(
+				GroupConstants.DEFAULT_PARENT_GROUP_ID,
+				portletDataContext.getCompanyId(), name,
+				ResourceConstants.SCOPE_GROUP_TEMPLATE,
+				String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
+				importedRole.getRoleId(), actionId);
+		}
+		else if (scope == ResourceConstants.SCOPE_GROUP) {
+			long groupId = portletDataContext.getCompanyGroupId();
+
+			if (sourceGroupId ==
+					portletDataContext.getSourceUserPersonalSiteGroupId()) {
+
+				groupId = portletDataContext.getUserPersonalSiteGroupId();
+			}
+
+			ResourcePermissionServiceUtil.addResourcePermission(
+				groupId, portletDataContext.getCompanyId(), name,
+				ResourceConstants.SCOPE_GROUP, String.valueOf(groupId),
+				importedRole.getRoleId(), actionId);
+		}
+		else {
+			if (_log.isDebugEnabled()) {
+				_log.debug(
+					"Individual scoped permission import not supported: " +
+						permission.toString());
+			}
+		}
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(
+		RoleStagedModelDataHandler.class);
+
+	private PermissionConversionFilter _permissionConversionFilter =
+		new ImportExportPermissionConversionFilter();
 
 }
