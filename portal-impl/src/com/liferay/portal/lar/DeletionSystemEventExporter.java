7aa2c7a84a502f334757e2a754bf249dc6392165
Zsolt Berentey <zsolt.berentey@liferay.com>
2013-06-20 17:21:52 +0200

    LPS-36216 Add extraData to the xml

--- portal-impl/src/com/liferay/portal/lar/DeletionSystemEventExporter.java
+++ portal-impl/src/com/liferay/portal/lar/DeletionSystemEventExporter.java
@@ -175,6 +175,9 @@ public class DeletionSystemEventExporter {
 		deletionSystemEventElement.addAttribute(
 			"uuid", systemEvent.getClassUuid());
 
+		deletionSystemEventElement.addAttribute(
+			"extra-data", systemEvent.getExtraData());
+
 		ManifestSummary manifestSummary =
 			portletDataContext.getManifestSummary();
 
