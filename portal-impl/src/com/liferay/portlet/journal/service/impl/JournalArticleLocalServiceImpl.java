7084ab66c99f53a197ed2189159b90c25b8bca69
Jonathan McCann <jonathan.mccann@liferay.com>
2014-02-14 07:37:55 -0800

    LPS-44246 Fix compile error

--- portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -5447,7 +5447,8 @@ public class JournalArticleLocalServiceImpl
 							serviceContext);
 
 					articleURL = setArticleURL(
-						articleURL, groupId, folderId, articleId);
+						articleURL, article.getGroupId(), article.getFolderId(),
+						article.getArticleId());
 
 					sendEmail(
 						article, articleURL, preferences, msg, serviceContext);
@@ -5774,9 +5775,6 @@ public class JournalArticleLocalServiceImpl
 				portletPreferencesLocalService.getPreferences(
 					article.getCompanyId(), ownerId, ownerType, plid,
 					portletId);
-
-			articleURL = setArticleURL(
-				articleURL, groupId, folderId, articleId);
 
 			sendEmail(
 				article, articleURL, preferences, "review",
@@ -6639,7 +6637,7 @@ public class JournalArticleLocalServiceImpl
 	}
 
 	protected String setArticleURL(
-		String articleURL, long groupId, String folderId, double articleId) {
+		String articleURL, long groupId, long folderId, String articleId) {
 
 		StringBundler sb = new StringBundler(7);
 
