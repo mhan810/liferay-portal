9d98ed5d4e4e90a490c4a50f5a88461d68acabf6
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-08-20 09:45:01 +0200

    Revert "LPS-49443 SF"
    This reverts commit b170b6ef5a642ce07f90759554060d2aa61168d9.

--- modules/portal/portal-search-elasticsearch/src/com/liferay/portal/search/elasticsearch/ElasticsearchIndexSearcher.java
+++ modules/portal/portal-search-elasticsearch/src/com/liferay/portal/search/elasticsearch/ElasticsearchIndexSearcher.java
@@ -441,9 +441,9 @@ public class ElasticsearchIndexSearcher extends BaseIndexSearcher {
 			return;
 		}
 
-		Map<String, Aggregation> aggregationsMap = aggregations.getAsMap();
-
 		Map<String, Facet> facetsMap = searchContext.getFacets();
+
+		Map<String, Aggregation> aggregationsMap = aggregations.getAsMap();
 
 		for (Facet facet : facetsMap.values()) {
 			if (facet.isStatic()) {
