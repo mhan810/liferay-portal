dffea8423cdfef49c914bbbe84e17a481b8082cb
Vilmos Papp <vilmos.papp@liferay.com>
2014-11-17 21:40:49 +0100

    LPS-49545 Added ANTLR grammar for LDAPFilter parsing based on RFC2254

--- portal-service/src/com/liferay/portal/kernel/ldap/LDAPUtil.java
+++ portal-service/src/com/liferay/portal/kernel/ldap/LDAPUtil.java
@@ -20,6 +20,7 @@ import com.liferay.portal.kernel.util.DateFormatFactoryUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.security.ldap.LDAPFilterValidator;
 
 import java.text.DateFormat;
 
