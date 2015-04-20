8072d27ab36e4ef79254a53991504bc74c219ed0
tinatian <tina.tian@liferay.com>
2015-04-20 10:04:09 +0800

    Add osgi Service tracker to PortalCacheProvider.

--- portal-impl/src/com/liferay/portal/cache/AbstractPortalCacheManager.java
+++ portal-impl/src/com/liferay/portal/cache/AbstractPortalCacheManager.java
@@ -25,7 +25,6 @@ import com.liferay.portal.kernel.cache.CallbackFactory;
 import com.liferay.portal.kernel.cache.PortalCache;
 import com.liferay.portal.kernel.cache.PortalCacheException;
 import com.liferay.portal.kernel.cache.PortalCacheManager;
-import com.liferay.portal.kernel.cache.PortalCacheProvider;
 import com.liferay.portal.kernel.cache.configuration.CallbackConfiguration;
 import com.liferay.portal.kernel.cache.configuration.PortalCacheConfiguration;
 import com.liferay.portal.kernel.cache.configuration.PortalCacheManagerConfiguration;
@@ -77,8 +76,6 @@ public abstract class AbstractPortalCacheManager<K extends Serializable, V>
 				registerCacheManagerListener(cacheManagerListener);
 			}
 		}
-
-		PortalCacheProvider.registerPortalCacheManager(this);
 	}
 
 	@Override
@@ -88,8 +85,6 @@ public abstract class AbstractPortalCacheManager<K extends Serializable, V>
 
 	@Override
 	public void destroy() {
-		PortalCacheProvider.unregisterPortalCacheManager(getName());
-
 		portalCaches.clear();
 
 		doDestroy();
