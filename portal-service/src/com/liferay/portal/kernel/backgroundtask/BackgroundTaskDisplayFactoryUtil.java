7f584dd22face76b983068185b58a610e94676ac
arbetts <andrew.betts@liferay.com>
2015-04-20 14:05:49 -0700

    LPS-54744 create display object to use for background task progress and status display

---
+++ portal-service/src/com/liferay/portal/kernel/backgroundtask/BackgroundTaskDisplayFactoryUtil.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.backgroundtask;
+
+import com.liferay.portal.kernel.security.pacl.permission.PortalRuntimePermission;
+
+import java.util.Locale;
+
+/**
+ * @author Andrew Betts
+ */
+public class BackgroundTaskDisplayFactoryUtil {
+
+	public static BackgroundTaskDisplay getBackgroundTaskDisplay(
+		long backgroundTaskId, Locale locale) {
+
+		return getBackgroundTaskDisplayFactory().getBackgroundTaskDisplay(
+			backgroundTaskId, locale);
+	}
+
+	public static BackgroundTaskDisplayFactory
+		getBackgroundTaskDisplayFactory() {
+
+		PortalRuntimePermission.checkGetBeanProperty(
+			BackgroundTaskStatusRegistryUtil.class);
+
+		return _backgroundTaskDisplayFactory;
+	}
+
+	public void setBackgroundTaskDisplayFactory(
+		BackgroundTaskDisplayFactory BackgroundTaskDisplayFactory) {
+
+		PortalRuntimePermission.checkSetBeanProperty(getClass());
+
+		_backgroundTaskDisplayFactory = BackgroundTaskDisplayFactory;
+	}
+
+	private static BackgroundTaskDisplayFactory _backgroundTaskDisplayFactory;
+
+}
