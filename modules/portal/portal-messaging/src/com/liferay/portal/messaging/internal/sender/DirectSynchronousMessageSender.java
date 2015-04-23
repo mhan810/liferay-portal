d002bdf4b070c9ebbb4920ab4e46d67e4e2db872
Michael Han <michael.han@liferay.com>
2015-04-23 18:52:24 -0500

    LPS-55189 add osgi bindings

--- modules/portal/portal-messaging/src/com/liferay/portal/messaging/internal/sender/DirectSynchronousMessageSender.java
+++ modules/portal/portal-messaging/src/com/liferay/portal/messaging/internal/sender/DirectSynchronousMessageSender.java
@@ -28,9 +28,16 @@ import com.liferay.portal.kernel.nio.intraband.messaging.IntrabandBridgeDestinat
 
 import java.util.Set;
 
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 /**
  * @author Shuyang Zhou
  */
+@Component(
+	immediate = true, property = { "mode=DIRECT" },
+	service = SynchronousMessageSender.class
+)
 public class DirectSynchronousMessageSender
 	implements SynchronousMessageSender {
 
@@ -84,7 +91,8 @@ public class DirectSynchronousMessageSender
 		return send(destinationName, message);
 	}
 
-	public void setMessageBus(MessageBus messageBus) {
+	@Reference(unbind = "-")
+	protected void setMessageBus(MessageBus messageBus) {
 		_messageBus = messageBus;
 	}
 
