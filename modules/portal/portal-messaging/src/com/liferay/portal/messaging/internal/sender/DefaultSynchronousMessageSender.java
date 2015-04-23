dc82c1c700e2034d9027f9215c9ba72ecda76fc5
Michael Han <michael.han@liferay.com>
2015-04-23 18:52:24 -0500

    LPS-55189 add osgi bindings

--- modules/portal/portal-messaging/src/com/liferay/portal/messaging/internal/sender/DefaultSynchronousMessageSender.java
+++ modules/portal/portal-messaging/src/com/liferay/portal/messaging/internal/sender/DefaultSynchronousMessageSender.java
@@ -23,29 +23,16 @@ import com.liferay.portal.kernel.messaging.MessageBus;
 import com.liferay.portal.kernel.messaging.MessageBusException;
 import com.liferay.portal.kernel.messaging.sender.SynchronousMessageListener;
 import com.liferay.portal.kernel.messaging.sender.SynchronousMessageSender;
+import com.liferay.portal.kernel.security.SecureRandomUtil;
 import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.kernel.uuid.PortalUUID;
+
+import java.util.UUID;
 
 /**
  * @author Michael C. Han
  */
 public class DefaultSynchronousMessageSender
 	implements SynchronousMessageSender {
-
-	public DefaultSynchronousMessageSender() {
-	}
-
-	/**
-	 * @deprecated As of 6.1.0
-	 */
-	@Deprecated
-	public DefaultSynchronousMessageSender(
-		MessageBus messageBus, PortalUUID portalUUID, long timeout) {
-
-		_messageBus = messageBus;
-		_portalUUID = portalUUID;
-		_timeout = timeout;
-	}
 
 	@Override
 	public Object send(String destinationName, Message message)
@@ -99,7 +86,7 @@ public class DefaultSynchronousMessageSender
 				DestinationNames.MESSAGE_BUS_DEFAULT_RESPONSE);
 		}
 
-		String responseId = _portalUUID.generate();
+		String responseId = generateUUUID();
 
 		message.setResponseId(responseId);
 
@@ -113,19 +100,21 @@ public class DefaultSynchronousMessageSender
 		_messageBus = messageBus;
 	}
 
-	public void setPortalUUID(PortalUUID portalUUID) {
-		_portalUUID = portalUUID;
-	}
-
 	public void setTimeout(long timeout) {
 		_timeout = timeout;
+	}
+
+	protected String generateUUUID() {
+		UUID uuid = new UUID(
+			SecureRandomUtil.nextLong(), SecureRandomUtil.nextLong());
+
+		return uuid.toString();
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		DefaultSynchronousMessageSender.class);
 
 	private MessageBus _messageBus;
-	private PortalUUID _portalUUID;
 	private long _timeout;
 
 }
