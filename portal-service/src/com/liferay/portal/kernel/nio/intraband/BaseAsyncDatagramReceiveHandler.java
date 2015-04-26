9631036997ecf9b462d5e2276d5a2b75170a27ca
Michael Han <michael.han@liferay.com>
2015-04-26 11:30:05 -0500

    LPS-55189 revert

--- portal-service/src/com/liferay/portal/kernel/nio/intraband/BaseAsyncDatagramReceiveHandler.java
+++ portal-service/src/com/liferay/portal/kernel/nio/intraband/BaseAsyncDatagramReceiveHandler.java
@@ -14,14 +14,9 @@
 
 package com.liferay.portal.kernel.nio.intraband;
 
-import com.liferay.portal.kernel.executor.PortalExecutorManager;
+import com.liferay.portal.kernel.executor.PortalExecutorManagerUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.registry.Registry;
-import com.liferay.registry.RegistryUtil;
-import com.liferay.registry.ServiceReference;
-import com.liferay.registry.ServiceTracker;
-import com.liferay.registry.ServiceTrackerCustomizer;
 
 import java.util.concurrent.Executor;
 
@@ -34,30 +29,13 @@ public abstract class BaseAsyncDatagramReceiveHandler
 	public BaseAsyncDatagramReceiveHandler() {
 		Class<? extends BaseAsyncDatagramReceiveHandler> clazz = getClass();
 
-		_className = clazz.getName();
-
-		Registry registry = RegistryUtil.getRegistry();
-
-		_serviceTracker = registry.trackServices(
-			PortalExecutorManager.class,
-			new PortalExecutorManagerServiceTrackerCustomizer());
-
-		_serviceTracker.open();
+		_executor = PortalExecutorManagerUtil.getPortalExecutor(
+			clazz.getName());
 	}
 
 	@Override
 	public void receive(
 		RegistrationReference registrationReference, Datagram datagram) {
-
-		if (_executor == null) {
-			if (_log.isWarnEnabled()) {
-				_log.warn(
-					"No executor to dispatch job: " + registrationReference +
-					" " + datagram);
-			}
-
-			return;
-		}
 
 		_executor.execute(new DispatchJob(registrationReference, datagram));
 	}
@@ -69,10 +47,7 @@ public abstract class BaseAsyncDatagramReceiveHandler
 	private static final Log _log = LogFactoryUtil.getLog(
 		BaseAsyncDatagramReceiveHandler.class);
 
-	private final String _className;
-	private Executor _executor;
-	private final ServiceTracker<PortalExecutorManager, PortalExecutorManager>
-		_serviceTracker;
+	private final Executor _executor;
 
 	private class DispatchJob implements Runnable {
 
@@ -95,38 +70,6 @@ public abstract class BaseAsyncDatagramReceiveHandler
 
 		private final Datagram _datagram;
 		private final RegistrationReference _registrationReference;
-
-	}
-
-	private class PortalExecutorManagerServiceTrackerCustomizer
-		implements ServiceTrackerCustomizer
-			<PortalExecutorManager, PortalExecutorManager> {
-
-		@Override
-		public PortalExecutorManager addingService(
-			ServiceReference<PortalExecutorManager> serviceReference) {
-
-			Registry registry = RegistryUtil.getRegistry();
-
-			PortalExecutorManager portalExecutorManager = registry.getService(
-				serviceReference);
-
-			_executor = portalExecutorManager.getPortalExecutor(_className);
-
-			return portalExecutorManager;
-		}
-
-		@Override
-		public void modifiedService(
-			ServiceReference<PortalExecutorManager> serviceReference,
-			PortalExecutorManager service) {
-		}
-
-		@Override
-		public void removedService(
-			ServiceReference<PortalExecutorManager> serviceReference,
-			PortalExecutorManager service) {
-		}
 
 	}
 
