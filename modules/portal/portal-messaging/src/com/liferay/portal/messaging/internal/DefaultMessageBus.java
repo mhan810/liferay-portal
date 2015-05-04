fef0d82ff041b66c37a33e7afeb7eb67b0e39b1d
Michael Han <michael.han@liferay.com>
2015-05-04 13:30:58 -0500

    LPS-55230 add osgi bindings for destinations and event listeners

--- modules/portal/portal-messaging/src/com/liferay/portal/messaging/internal/DefaultMessageBus.java
+++ modules/portal/portal-messaging/src/com/liferay/portal/messaging/internal/DefaultMessageBus.java
@@ -28,7 +28,13 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import com.liferay.portal.kernel.util.MapUtil;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Michael C. Han
@@ -37,6 +43,12 @@ import org.osgi.service.component.annotations.Component;
 public class DefaultMessageBus implements MessageBus {
 
 	@Override
+	@Reference(
+		cardinality = ReferenceCardinality.MULTIPLE,
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY,
+		target = "(destination.name=*)"
+	)
 	public synchronized void addDestination(Destination destination) {
 		_destinations.put(destination.getName(), destination);
 
@@ -183,6 +195,58 @@ public class DefaultMessageBus implements MessageBus {
 		}
 
 		return destination.unregister(messageListener);
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		shutdown(true);
+
+		_messageBusEventListeners.clear();
+	}
+
+	@Reference(
+		cardinality = ReferenceCardinality.MULTIPLE,
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY
+	)
+	protected void registerMessageBusEventListener(
+		MessageBusEventListener messageBusEventListener) {
+
+		addMessageBusEventListener(messageBusEventListener);
+	}
+
+	@Reference(
+		cardinality = ReferenceCardinality.MULTIPLE,
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY,
+		target = "(destination.name=*)"
+	)
+	protected void registerMessageListener(
+		MessageListener messageListener, Map<String, Object> properties) {
+
+		String destinationName = MapUtil.getString(
+			properties, "destination.name");
+
+		registerMessageListener(destinationName, messageListener);
+	}
+
+	protected void unregisterMessageListener(
+		MessageListener messageListener, Map<String, Object> properties) {
+
+		String destinationName = MapUtil.getString(
+			properties, "destination.name");
+
+		unregisterMessageListener(destinationName, messageListener);
+	}
+
+	protected synchronized void removeDestination(Destination destination) {
+		removeDestination(destination.getName());
+	}
+
+	protected void unregisterMessageBusEventListener(
+		MessageBusEventListener messageBusEventListener) {
+
+		removeMessageBusEventListener(messageBusEventListener);
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
