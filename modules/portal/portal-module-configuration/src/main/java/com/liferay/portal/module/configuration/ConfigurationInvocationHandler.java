c604a2984b2deab189ff36b2a4b8557bcbcf3ff7
Michael Han <michael.han@liferay.com>
2016-01-04 08:55:27 -0600

    LPS-61440 support enums in settings

--- modules/portal/portal-module-configuration/src/main/java/com/liferay/portal/module/configuration/ConfigurationInvocationHandler.java
+++ modules/portal/portal-module-configuration/src/main/java/com/liferay/portal/module/configuration/ConfigurationInvocationHandler.java
@@ -107,6 +107,13 @@ public class ConfigurationInvocationHandler<S> implements InvocationHandler {
 		else if (returnType.equals(String[].class)) {
 			return _typedSettings.getValues(method.getName());
 		}
+		else if (returnType.isEnum()) {
+			Method valueOfMethod = returnType.getMethod(
+				"valueOf", Enum.class, String.class);
+
+			return valueOfMethod.invoke(
+				Enum.class, _typedSettings.getValue(method.getName()));
+		}
 
 		Constructor<?> constructor = returnType.getConstructor(String.class);
 
