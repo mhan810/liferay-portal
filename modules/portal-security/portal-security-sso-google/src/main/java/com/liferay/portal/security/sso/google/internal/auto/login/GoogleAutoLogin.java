349fd144f025faabe140cf2fb940699499125207
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2016-01-26 10:19:18 +0000

    LPS-62396 Migrate from PrefsPropsUtil to settings API

--- modules/portal-security/portal-security-sso-google/src/main/java/com/liferay/portal/security/sso/google/internal/auto/login/GoogleAutoLogin.java
+++ modules/portal-security/portal-security-sso-google/src/main/java/com/liferay/portal/security/sso/google/internal/auto/login/GoogleAutoLogin.java
@@ -18,9 +18,9 @@ import com.liferay.portal.kernel.security.auto.login.AutoLogin;
 import com.liferay.portal.kernel.security.auto.login.BaseAutoLogin;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
-import com.liferay.portal.kernel.util.PrefsPropsUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.User;
+import com.liferay.portal.security.sso.google.GoogleAuthorization;
 import com.liferay.portal.security.sso.google.constants.GoogleWebKeys;
 import com.liferay.portal.service.UserLocalService;
 
@@ -44,10 +44,7 @@ public class GoogleAutoLogin extends BaseAutoLogin {
 
 		long companyId = PortalUtil.getCompanyId(request);
 
-		boolean googleAuthEnabled = PrefsPropsUtil.getBoolean(
-			companyId, "google-auth-enabled", true);
-
-		if (!googleAuthEnabled) {
+		if (!_googleAuthorization.isEnabled(companyId)) {
 			return null;
 		}
 
@@ -94,10 +91,18 @@ public class GoogleAutoLogin extends BaseAutoLogin {
 	}
 
 	@Reference(unbind = "-")
+	protected void setGoogleAuthorization(
+		GoogleAuthorization googleAuthorization) {
+
+		_googleAuthorization = googleAuthorization;
+	}
+
+	@Reference(unbind = "-")
 	protected void setUserLocalService(UserLocalService userLocalService) {
 		_userLocalService = userLocalService;
 	}
 
+	private GoogleAuthorization _googleAuthorization;
 	private UserLocalService _userLocalService;
 
 }
