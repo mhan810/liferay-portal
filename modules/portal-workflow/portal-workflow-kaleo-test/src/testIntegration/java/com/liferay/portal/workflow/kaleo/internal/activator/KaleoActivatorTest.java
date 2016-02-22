9fa4c96bcdd6b3b9de53856fd10958623c4a7222
Michael Han <michael.han@liferay.com>
2016-02-21 21:30:54 -0600

    LPS-63470 move test back to test module

--- modules/portal-workflow/portal-workflow-kaleo-test/src/testIntegration/java/com/liferay/portal/workflow/kaleo/internal/activator/KaleoActivatorTest.java
+++ modules/portal-workflow/portal-workflow-kaleo-test/src/testIntegration/java/com/liferay/portal/workflow/kaleo/internal/activator/KaleoActivatorTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.workflow.kaleo.runtime.internal.activator.test;
+package com.liferay.portal.workflow.kaleo.internal.activator;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.model.Company;
