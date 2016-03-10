ab173462c9ce6200e7792c8cbbc8725901c4a802
arbetts <andrew.betts@liferay.com>
2016-03-10 14:45:53 -0800

    LPS-52177 force indexer to be synced during deletion to prevent indexer errors

--- portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
@@ -498,16 +498,19 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 
 		Long currentCompanyId = CompanyThreadLocal.getCompanyId();
 		boolean deleteInProcess = CompanyThreadLocal.isDeleteInProcess();
+		boolean forceSync = ProxyModeThreadLocal.isForceSync();
 
 		try {
 			CompanyThreadLocal.setCompanyId(companyId);
 			CompanyThreadLocal.setDeleteInProcess(true);
+			ProxyModeThreadLocal.setForceSync(true);
 
 			return doDeleteCompany(companyId);
 		}
 		finally {
 			CompanyThreadLocal.setCompanyId(currentCompanyId);
 			CompanyThreadLocal.setDeleteInProcess(deleteInProcess);
+			ProxyModeThreadLocal.setForceSync(forceSync);
 		}
 	}
 
