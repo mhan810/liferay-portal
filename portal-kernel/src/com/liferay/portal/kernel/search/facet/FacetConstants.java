9740cccfcdef79dee0a49da3c134c78514c246cd
André de Oliveira <andre.oliveira@liferay.com>
2016-03-18 17:34:08 -0300

    LPS-64398 New FacetManager, FacetedSearcher interfaces in portal-kernel
    https://issues.liferay.com/browse/LPS-64398

---
+++ portal-kernel/src/com/liferay/portal/kernel/search/facet/FacetConstants.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.search.facet;
+
+/**
+ * @author André de Oliveira
+ */
+public class FacetConstants {
+
+	public static final String ASSET_CATEGORY_IDS_FACET =
+		"AssetCategoryIdsFacet";
+
+	public static final String ASSET_ENTRIES_FACET = "AssetEntriesFacet";
+
+	public static final String ASSET_TAG_NAMES_FACET = "AssetTagNamesFacet";
+
+	public static final String LAYOUT_UUID_FACET = "LayoutUuidFacet";
+
+	public static final String SCOPE_FACET = "ScopeFacet";
+
+	public static final String TREE_PATH_FACET = "TreePathFacet";
+
+	public static final String USER_ID_FACET = "UserIdFacet";
+
+}
