ff174c7af2d9b158616b04cc65c1b6a10a741627
André de Oliveira <andre.oliveira@liferay.com>
2016-04-23 17:11:53 -0300

    LPS-65121 ProxyFactory null on redeploy, so use injection in modules
    https://issues.liferay.com/browse/LPS-65121

--- portal-kernel/src/com/liferay/portal/kernel/search/AbstractSearchEngineConfigurator.java
+++ portal-kernel/src/com/liferay/portal/kernel/search/AbstractSearchEngineConfigurator.java
@@ -90,7 +90,9 @@ public abstract class AbstractSearchEngineConfigurator
 		_searchEngineRegistrations.clear();
 
 		if (Validator.isNotNull(_originalSearchEngineId)) {
-			SearchEngineHelperUtil.setDefaultSearchEngineId(
+			SearchEngineHelper searchEngineHelper = getSearchEngineHelper();
+
+			searchEngineHelper.setDefaultSearchEngineId(
 				_originalSearchEngineId);
 
 			_originalSearchEngineId = null;
@@ -189,7 +191,9 @@ public abstract class AbstractSearchEngineConfigurator
 		_messageBus.removeDestination(
 			searchEngineRegistration.getSearchWriterDestinationName());
 
-		SearchEngineHelperUtil.removeSearchEngine(
+		SearchEngineHelper searchEngineHelper = getSearchEngineHelper();
+
+		searchEngineHelper.removeSearchEngine(
 			searchEngineRegistration.getSearchEngineId());
 
 		if (!searchEngineRegistration.isOverride()) {
@@ -213,7 +217,7 @@ public abstract class AbstractSearchEngineConfigurator
 			searchWriterDestination,
 			searchEngineRegistration.getOriginalSearchWriterMessageListeners());
 
-		SearchEngineHelperUtil.setSearchEngine(
+		searchEngineHelper.setSearchEngine(
 			searchEngineRegistration.getSearchEngineId(),
 			originalSearchEngineProxy);
 	}
@@ -226,12 +230,15 @@ public abstract class AbstractSearchEngineConfigurator
 
 	protected abstract ClassLoader getOperatingClassloader();
 
+	protected abstract SearchEngineHelper getSearchEngineHelper();
+
 	protected Destination getSearchReaderDestination(
 		MessageBus messageBus, String searchEngineId) {
 
+		SearchEngineHelper searchEngineHelper = getSearchEngineHelper();
+
 		String searchReaderDestinationName =
-			SearchEngineHelperUtil.getSearchReaderDestinationName(
-				searchEngineId);
+			searchEngineHelper.getSearchReaderDestinationName(searchEngineId);
 
 		Destination searchReaderDestination = messageBus.getDestination(
 			searchReaderDestinationName);
@@ -249,9 +256,10 @@ public abstract class AbstractSearchEngineConfigurator
 	protected Destination getSearchWriterDestination(
 		MessageBus messageBus, String searchEngineId) {
 
+		SearchEngineHelper searchEngineHelper = getSearchEngineHelper();
+
 		String searchWriterDestinationName =
-			SearchEngineHelperUtil.getSearchWriterDestinationName(
-				searchEngineId);
+			searchEngineHelper.getSearchWriterDestinationName(searchEngineId);
 
 		Destination searchWriterDestination = messageBus.getDestination(
 			searchWriterDestinationName);
@@ -276,11 +284,12 @@ public abstract class AbstractSearchEngineConfigurator
 		String defaultSearchEngineId = getDefaultSearchEngineId();
 
 		if (Validator.isNotNull(defaultSearchEngineId)) {
-			_originalSearchEngineId =
-				SearchEngineHelperUtil.getDefaultSearchEngineId();
+			SearchEngineHelper searchEngineHelper = getSearchEngineHelper();
 
-			SearchEngineHelperUtil.setDefaultSearchEngineId(
-				defaultSearchEngineId);
+			_originalSearchEngineId =
+				searchEngineHelper.getDefaultSearchEngineId();
+
+			searchEngineHelper.setDefaultSearchEngineId(defaultSearchEngineId);
 		}
 
 		_searchEngines.clear();
@@ -306,8 +315,10 @@ public abstract class AbstractSearchEngineConfigurator
 		searchEngineRegistration.setSearchWriterDestinationName(
 			searchWriterDestination.getName());
 
+		SearchEngineHelper searchEngineHelper = getSearchEngineHelper();
+
 		SearchEngine originalSearchEngine =
-			SearchEngineHelperUtil.getSearchEngineSilent(searchEngineId);
+			searchEngineHelper.getSearchEngineSilent(searchEngineId);
 
 		if (originalSearchEngine != null) {
 			searchEngineRegistration.setOverride(true);
@@ -340,7 +351,7 @@ public abstract class AbstractSearchEngineConfigurator
 			new SearchEngineProxyWrapper(
 				searchEngine, getIndexSearcher(), getIndexWriter());
 
-		SearchEngineHelperUtil.setSearchEngine(
+		searchEngineHelper.setSearchEngine(
 			searchEngineId, searchEngineProxyWrapper);
 	}
 
