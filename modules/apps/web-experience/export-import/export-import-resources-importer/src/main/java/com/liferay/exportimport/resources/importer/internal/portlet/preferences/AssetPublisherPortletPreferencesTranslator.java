8b5beade09876aa8fdec970f50036e5a2c11eea7
briangreenwald <brian.greenwald@liferay.com>
2016-05-24 16:13:58 -0500

    LPS-66076 Add support for Asset Publisher and Nav Menu Portlets

---
+++ modules/apps/web-experience/export-import/export-import-resources-importer/src/main/java/com/liferay/exportimport/resources/importer/internal/portlet/preferences/AssetPublisherPortletPreferencesTranslator.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.exportimport.resources.importer.internal.portlet.preferences;
+
+import com.liferay.exportimport.resources.importer.portlet.preferences.PortletPreferencesTranslator;
+import com.liferay.portal.kernel.json.JSONObject;
+
+import javax.portlet.PortletException;
+import javax.portlet.PortletPreferences;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Brian Greenwald
+ */
+@Component(
+	immediate = true,
+	property = {"portlet.preferences.translator.portlet.id=com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet"},
+	service = PortletPreferencesTranslator.class
+)
+public class AssetPublisherPortletPreferencesTranslator
+	implements PortletPreferencesTranslator {
+
+	@Override
+	public void translate(
+			JSONObject portletPreferencesJSONObject, String key,
+			PortletPreferences portletPreferences)
+		throws PortletException {
+
+		_portletPreferencesTranslator.translate(
+			portletPreferencesJSONObject, key, portletPreferences);
+	}
+
+	@Reference(target = "(portlet.preferences.translator=com.liferay.exportimport.resources.importer.internal.portlet.preferences.BasicPortletPreferencesTranslator)")
+	private PortletPreferencesTranslator _portletPreferencesTranslator;
+
+}
