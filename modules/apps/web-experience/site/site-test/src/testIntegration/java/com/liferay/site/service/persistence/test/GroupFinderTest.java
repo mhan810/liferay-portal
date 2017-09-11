5acb082752204ec39db16535433452dc23033b76
Alan Huang <alan.huang@liferay.com>
2017-09-11 16:48:03 +0800

    LPS-74258 assertTrue->assertFalse

--- modules/apps/web-experience/site/site-test/src/testIntegration/java/com/liferay/site/service/persistence/test/GroupFinderTest.java
+++ modules/apps/web-experience/site/site-test/src/testIntegration/java/com/liferay/site/service/persistence/test/GroupFinderTest.java
@@ -293,7 +293,7 @@ public class GroupFinderTest {
 		List<Long> groups = GroupFinderUtil.findByUserActiveGroupIds(
 			TestPropsValues.getUserId());
 
-		Assert.assertTrue(groups.isEmpty());
+		Assert.assertFalse(groups.isEmpty());
 	}
 
 	protected static ResourceAction getModelResourceAction()
