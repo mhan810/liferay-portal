712958986f5279b751931596e320f9df6e2f6c38
Michael Han <michael.han@liferay.com>
2017-10-06 10:44:09 -0500

    LPS-78255 document-library-service: integrate DLFileEntry into model indexer
    https://issues.liferay.com/browse/LPS-78255

---
+++ modules/apps/collaboration/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/search/contributor/model/DLFileEntryModelSearchConfiguratorPublisher.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.internal.search.contributor.model;
+
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.document.library.kernel.model.DLFolder;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.portal.search.contributor.model.ModelIndexerWriterContributor;
+import com.liferay.portal.search.contributor.model.ModelSearchConfigurator;
+import com.liferay.portal.search.contributor.model.ModelSearchSettings;
+import com.liferay.portal.search.contributor.model.ModelSummaryContributor;
+
+import java.util.Dictionary;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Michael C. Han
+ */
+@Component(
+	immediate = true,
+	service = DLFileEntryModelSearchConfiguratorPublisher.class
+)
+public class DLFileEntryModelSearchConfiguratorPublisher {
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		ModelSearchSettings modelSearchSettings = new ModelSearchSettings(
+			DLFileEntry.class.getName());
+
+		modelSearchSettings.setDefaultSelectedFieldNames(
+			Field.ASSET_TAG_NAMES, Field.COMPANY_ID, Field.CONTENT,
+			Field.ENTRY_CLASS_NAME, Field.ENTRY_CLASS_PK, Field.GROUP_ID,
+			Field.MODIFIED_DATE, Field.SCOPE_GROUP_ID, Field.TITLE, Field.UID);
+
+		ModelSearchConfigurator<DLFolder> modelSearchConfigurator =
+			new ModelSearchConfigurator<>(
+				bundleContext, modelIndexWriterContributor, modelSearchSettings,
+				modelSummaryContributor);
+
+		Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+		properties.put(
+			"indexer.class.name", modelSearchSettings.getClassName());
+
+		_serviceRegistration = bundleContext.registerService(
+			ModelSearchConfigurator.class, modelSearchConfigurator, properties);
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		if (_serviceRegistration != null) {
+			_serviceRegistration.unregister();
+		}
+	}
+
+	@Reference(
+		target = "(indexer.class.name=com.liferay.document.library.kernel.model.DLFileEntry)"
+	)
+	protected ModelIndexerWriterContributor<DLFolder>
+		modelIndexWriterContributor;
+
+	@Reference(
+		target = "(indexer.class.name=com.liferay.document.library.kernel.model.DLFileEntry)"
+	)
+	protected ModelSummaryContributor modelSummaryContributor;
+
+	private ServiceRegistration<ModelSearchConfigurator> _serviceRegistration;
+
+}
