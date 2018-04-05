bd2bfb85d7061f03d1d82ae80a5c2d201c393b97
Eric Yan <eric.yan@liferay.com>
2018-04-05 16:56:54 -0700

    LPS-XXXXX portal-search: create base permission document using the basemodel's indexer

--- modules/apps/foundation/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/permission/SearchPermissionIndexWriterImpl.java
+++ modules/apps/foundation/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/permission/SearchPermissionIndexWriterImpl.java
@@ -14,12 +14,23 @@
 
 package com.liferay.portal.search.internal.permission;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.BaseModel;
 import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.DocumentImpl;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistry;
+import com.liferay.portal.kernel.search.SearchException;
+import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.search.index.UpdateDocumentIndexWriter;
 import com.liferay.portal.search.indexer.BaseModelDocumentFactory;
 import com.liferay.portal.search.permission.SearchPermissionDocumentContributor;
 import com.liferay.portal.search.permission.SearchPermissionIndexWriter;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -36,17 +47,78 @@ public class SearchPermissionIndexWriterImpl
 		BaseModel<?> baseModel, long companyId, String searchEngineId,
 		boolean commitImmediately) {
 
-		Document document = baseModelDocumentFactory.createDocument(baseModel);
+		Document document = createBasePermissionDocument(baseModel);
 
-		searchPermissionDocumentContributor.addPermissionFields(
-			companyId, document);
+		if (document != null) {
+			searchPermissionDocumentContributor.addPermissionFields(
+				companyId, document);
 
-		updateDocumentIndexWriter.updateDocumentPartially(
-			searchEngineId, companyId, document, commitImmediately);
+			updateDocumentIndexWriter.updateDocumentPartially(
+				searchEngineId, companyId, document, commitImmediately);
+		}
+		else {
+			_log.error(
+				StringBundler.concat(
+					"Unable to update permissions for ",
+					baseModel.getModelClassName(), " with primaryKey ",
+					String.valueOf(baseModel.getPrimaryKeyObj())),
+				new Exception());
+		}
+	}
+
+	protected Document createBasePermissionDocument(BaseModel<?> baseModel) {
+		Indexer<BaseModel<?>> indexer = indexerRegistry.getIndexer(
+			baseModel.getModelClassName());
+
+		if (indexer != null) {
+			try {
+				Document indexerDocument = indexer.getDocument(baseModel);
+				Document document = new DocumentImpl();
+
+				document.addKeyword(
+					Field.ENTRY_CLASS_NAME,
+					indexerDocument.get(Field.ENTRY_CLASS_NAME));
+				document.addKeyword(
+					Field.ENTRY_CLASS_PK,
+					indexerDocument.get(Field.ENTRY_CLASS_PK));
+				document.addKeyword(
+					Field.GROUP_ID, indexerDocument.get(Field.GROUP_ID));
+				document.addKeyword(Field.UID, indexerDocument.get(Field.UID));
+
+				List<String> relatedPermissionFieldNames = Arrays.asList(
+					Field.RELATED_ENTRY, Field.CLASS_NAME_ID, Field.CLASS_PK);
+
+				for (String relatedPermissionFieldName :
+						relatedPermissionFieldNames) {
+
+					if (document.hasField(relatedPermissionFieldName)) {
+						document.addKeyword(
+							relatedPermissionFieldName,
+							indexerDocument.get(relatedPermissionFieldName));
+					}
+				}
+
+				return document;
+			}
+			catch (SearchException se) {
+				if (_log.isWarnEnabled()) {
+					_log.warn(
+						StringBundler.concat(
+							"Unable to create document with indexer for ",
+							baseModel.getModelClassName()),
+						se);
+				}
+			}
+		}
+
+		return null;
 	}
 
 	@Reference
 	protected BaseModelDocumentFactory baseModelDocumentFactory;
+
+	@Reference
+	protected IndexerRegistry indexerRegistry;
 
 	@Reference
 	protected SearchPermissionDocumentContributor
@@ -54,5 +126,8 @@ public class SearchPermissionIndexWriterImpl
 
 	@Reference
 	protected UpdateDocumentIndexWriter updateDocumentIndexWriter;
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		SearchPermissionIndexWriterImpl.class);
 
 }
