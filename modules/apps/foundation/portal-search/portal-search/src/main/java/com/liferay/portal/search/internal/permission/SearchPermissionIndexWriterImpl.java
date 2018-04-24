1e9fdaa89184c49921754389adf3544ba4be04d9
Eric Yan <eric.yan@liferay.com>
2018-04-24 15:54:01 -0700

    LPS-XXXXX portal-search: add search permission model document contributor service tracker customizer

--- modules/apps/foundation/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/permission/SearchPermissionIndexWriterImpl.java
+++ modules/apps/foundation/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/permission/SearchPermissionIndexWriterImpl.java
@@ -14,15 +14,29 @@
 
 package com.liferay.portal.search.internal.permission;
 
+import com.liferay.osgi.util.ServiceTrackerFactory;
 import com.liferay.portal.kernel.model.BaseModel;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.search.index.UpdateDocumentIndexWriter;
 import com.liferay.portal.search.indexer.BaseModelDocumentFactory;
 import com.liferay.portal.search.permission.SearchPermissionDocumentContributor;
 import com.liferay.portal.search.permission.SearchPermissionIndexWriter;
+import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
+import com.liferay.portal.search.spi.model.index.contributor.SearchPermissionModelDocumentContributor;
 
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
  * @author Michael C. Han
@@ -45,6 +59,20 @@ public class SearchPermissionIndexWriterImpl
 			searchEngineId, companyId, document, commitImmediately);
 	}
 
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundleContext = bundleContext;
+
+		_serviceTracker = ServiceTrackerFactory.open(
+			bundleContext, SearchPermissionModelDocumentContributor.class,
+			new SearchPermissionModelDocumentContributorTrackerCustomizer());
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTracker.close();
+	}
+
 	@Reference
 	protected BaseModelDocumentFactory baseModelDocumentFactory;
 
@@ -54,5 +82,104 @@ public class SearchPermissionIndexWriterImpl
 
 	@Reference
 	protected UpdateDocumentIndexWriter updateDocumentIndexWriter;
+
+	private BundleContext _bundleContext;
+	private final CopyOnWriteArrayList<SearchPermissionModelDocumentContributor>
+		_defaultSearchPermissionModelDocumentContributors =
+			new CopyOnWriteArrayList<>();
+	private final Map
+		<String, CopyOnWriteArrayList<SearchPermissionModelDocumentContributor>>
+			_searchPermissionModelDocumentContributorMap =
+				new ConcurrentHashMap<>();
+	private ServiceTracker
+		<SearchPermissionModelDocumentContributor,
+			SearchPermissionModelDocumentContributor> _serviceTracker;
+
+	private class
+		SearchPermissionModelDocumentContributorTrackerCustomizer
+			implements ServiceTrackerCustomizer
+				<SearchPermissionModelDocumentContributor,
+					SearchPermissionModelDocumentContributor> {
+
+		@Override
+		public SearchPermissionModelDocumentContributor addingService(
+			ServiceReference<SearchPermissionModelDocumentContributor>
+				serviceReference) {
+
+			SearchPermissionModelDocumentContributor
+				searchPermissionModelDocumentContributor =
+					_bundleContext.getService(serviceReference);
+
+			if (searchPermissionModelDocumentContributor instanceof
+					ModelDocumentContributor) {
+
+				String indexerClassName = (String)serviceReference.getProperty(
+					"indexer.class.name");
+
+				if (indexerClassName != null) {
+					if (!_searchPermissionModelDocumentContributorMap.
+							containsKey(indexerClassName)) {
+
+						_searchPermissionModelDocumentContributorMap.put(
+							indexerClassName, new CopyOnWriteArrayList<>());
+					}
+
+					List<SearchPermissionModelDocumentContributor>
+						searchPermissionModelDocumentContributors =
+							_searchPermissionModelDocumentContributorMap.get(
+								indexerClassName);
+
+					searchPermissionModelDocumentContributors.add(
+						searchPermissionModelDocumentContributor);
+				}
+			}
+			else {
+				_defaultSearchPermissionModelDocumentContributors.add(
+					searchPermissionModelDocumentContributor);
+			}
+
+			return searchPermissionModelDocumentContributor;
+		}
+
+		@Override
+		public void modifiedService(
+			ServiceReference<SearchPermissionModelDocumentContributor>
+				serviceReference,
+			SearchPermissionModelDocumentContributor
+				searchPermissionModelDocumentContributor) {
+		}
+
+		@Override
+		public void removedService(
+			ServiceReference<SearchPermissionModelDocumentContributor>
+				serviceReference,
+			SearchPermissionModelDocumentContributor
+				searchPermissionModelDocumentContributor) {
+
+			if (searchPermissionModelDocumentContributor instanceof
+					ModelDocumentContributor) {
+
+				String indexerClassName = (String)serviceReference.getProperty(
+					"indexer.class.name");
+
+				if (indexerClassName != null) {
+					List<SearchPermissionModelDocumentContributor>
+						searchPermissionModelDocumentContributors =
+							_searchPermissionModelDocumentContributorMap.get(
+								indexerClassName);
+
+					if (searchPermissionModelDocumentContributors != null) {
+						searchPermissionModelDocumentContributors.remove(
+							searchPermissionModelDocumentContributor);
+					}
+				}
+			}
+			else {
+				_defaultSearchPermissionModelDocumentContributors.remove(
+					searchPermissionModelDocumentContributor);
+			}
+		}
+
+	}
 
 }
