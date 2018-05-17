669dcaa98b1c31235282fe3717af59b3dc3691db
Michael Han <michael.han@liferay.com>
2018-05-17 13:09:41 -0500

    LPS-80976 add document tests

---
+++ modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/test/java/com/liferay/portal/search/elasticsearch6/internal/adapter/document/DocumentRequestExecutorFixture.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.elasticsearch6.internal.adapter.document;
+
+import com.liferay.portal.search.elasticsearch6.internal.connection.ElasticsearchConnectionManager;
+
+/**
+ * @author Dylan Rebelak
+ */
+public class DocumentRequestExecutorFixture {
+
+	public DocumentRequestExecutorFixture(
+		ElasticsearchConnectionManager elasticsearchConnectionManager) {
+
+		_elasticsearchConnectionManager = elasticsearchConnectionManager;
+	}
+
+	public ElasticsearchDocumentRequestExecutor createExecutor() {
+		ElasticsearchDocumentRequestExecutor documentRequestExecutor =
+			new ElasticsearchDocumentRequestExecutor();
+
+		DeleteByQueryDocumentRequestExecutorImpl
+			deleteByQueryDocumentRequestExecutorImpl =
+				new DeleteByQueryDocumentRequestExecutorImpl();
+
+		deleteByQueryDocumentRequestExecutorImpl.
+			elasticsearchConnectionManager = _elasticsearchConnectionManager;
+
+		documentRequestExecutor.deleteByQueryDocumentRequestExecutor =
+			deleteByQueryDocumentRequestExecutorImpl;
+
+		DeleteDocumentRequestExecutorImpl deleteDocumentRequestExecutorImpl =
+			new DeleteDocumentRequestExecutorImpl();
+
+		deleteDocumentRequestExecutorImpl.elasticsearchConnectionManager =
+			_elasticsearchConnectionManager;
+
+		documentRequestExecutor.deleteDocumentRequestExecutor =
+			deleteDocumentRequestExecutorImpl;
+
+		IndexDocumentRequestExecutorImpl indexDocumentRequestExecutorImpl =
+			new IndexDocumentRequestExecutorImpl();
+
+		indexDocumentRequestExecutorImpl.elasticsearchConnectionManager =
+			_elasticsearchConnectionManager;
+
+		documentRequestExecutor.indexDocumentRequestExecutor =
+			indexDocumentRequestExecutorImpl;
+
+		UpdateByQueryDocumentRequestExecutorImpl
+			updateByQueryDocumentRequestExecutorImpl =
+				new UpdateByQueryDocumentRequestExecutorImpl();
+
+		updateByQueryDocumentRequestExecutorImpl.
+			elasticsearchConnectionManager = _elasticsearchConnectionManager;
+
+		documentRequestExecutor.updateByQueryDocumentRequestExecutor =
+			updateByQueryDocumentRequestExecutorImpl;
+
+		UpdateDocumentRequestExecutorImpl updateDocumentRequestExecutorImpl =
+			new UpdateDocumentRequestExecutorImpl();
+
+		updateDocumentRequestExecutorImpl.elasticsearchConnectionManager =
+			_elasticsearchConnectionManager;
+
+		documentRequestExecutor.updateDocumentRequestExecutor =
+			updateDocumentRequestExecutorImpl;
+
+		return documentRequestExecutor;
+	}
+
+	private final ElasticsearchConnectionManager
+		_elasticsearchConnectionManager;
+
+}
