eef2154505b67bbf50c4888129dea2c925159962
Michael Han <michael.han@liferay.com>
2018-05-21 15:23:19 -0500

    LPS-80976 add cluster requests

---
+++ modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/main/java/com/liferay/portal/search/elasticsearch6/internal/adapter/cluster/ClusterHealthStatusTranslator.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.elasticsearch6.internal.adapter.cluster;
+
+import com.liferay.portal.search.engine.adapter.cluster.ClusterHealthStatus;
+
+/**
+ * @author Michael C. Han
+ */
+public class ClusterHealthStatusTranslator {
+
+	public static ClusterHealthStatus translate(
+		org.elasticsearch.cluster.health.ClusterHealthStatus
+			clusterHealthStatus) {
+
+		if (clusterHealthStatus ==
+				org.elasticsearch.cluster.health.ClusterHealthStatus.GREEN) {
+
+			return ClusterHealthStatus.GREEN;
+		}
+
+		if (clusterHealthStatus ==
+				org.elasticsearch.cluster.health.ClusterHealthStatus.RED) {
+
+			return ClusterHealthStatus.RED;
+		}
+
+		if (clusterHealthStatus ==
+				org.elasticsearch.cluster.health.ClusterHealthStatus.YELLOW) {
+
+			return ClusterHealthStatus.YELLOW;
+		}
+
+		throw new IllegalArgumentException(
+			"Unknown status: " + clusterHealthStatus);
+	}
+
+}
