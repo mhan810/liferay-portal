ad1c4cb1eb010c475e54744009ca37ce0cd5f055
Michael Han <michael.han@liferay.com>
2019-01-23 10:27:35 -0600

    LPS-89500 add new query API

---
+++ modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/query/TermsSetQuery.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.query;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.petra.string.StringBundler;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Michael C. Han
+ */
+@ProviderType
+public class TermsSetQuery extends BaseQueryImpl implements Query {
+
+	public TermsSetQuery(String fieldName, List<Object> values) {
+		_fieldName = fieldName;
+		_values = values;
+	}
+
+	@Override
+	public <T> T accept(QueryVisitor<T> queryVisitor) {
+		return queryVisitor.visit(this);
+	}
+
+	public String getExecutionOption() {
+		return null;
+	}
+
+	public String getFieldName() {
+		return _fieldName;
+	}
+
+	public String getMinimumShouldMatchField() {
+		return _minimumShouldMatchField;
+	}
+
+	public int getSortOrder() {
+		return 4;
+	}
+
+	public List<Object> getValues() {
+		return Collections.unmodifiableList(_values);
+	}
+
+	public Boolean isCached() {
+		return _cached;
+	}
+
+	public void setCached(Boolean cached) {
+		_cached = cached;
+	}
+
+	public void setExecutionOption(String executionOption) {
+	}
+
+	public String setMinimumShouldMatchField(String minimumShouldMatchField) {
+		return _minimumShouldMatchField = minimumShouldMatchField;
+	}
+
+	@Override
+	public String toString() {
+		return StringBundler.concat(
+			"{(", _fieldName, "=", _values, "), (minimum_should_match_field=",
+			_minimumShouldMatchField, ")}");
+	}
+
+	private Boolean _cached = Boolean.TRUE;
+	private final String _fieldName;
+	private String _minimumShouldMatchField;
+	private final List<Object> _values;
+
+}
