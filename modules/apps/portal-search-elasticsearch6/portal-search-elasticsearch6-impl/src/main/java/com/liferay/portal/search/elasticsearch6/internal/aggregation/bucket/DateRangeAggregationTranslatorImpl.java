c6d2e6126f2c8b44e186f2864c34b22348c626a6
Michael Han <michael.han@liferay.com>
2019-01-23 10:38:59 -0600

    LPS-89501 add translation impl for aggregations

---
+++ modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/main/java/com/liferay/portal/search/elasticsearch6/internal/aggregation/bucket/DateRangeAggregationTranslatorImpl.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.elasticsearch6.internal.aggregation.bucket;
+
+import com.liferay.portal.search.aggregation.AggregationTranslator;
+import com.liferay.portal.search.aggregation.bucket.DateRangeAggregation;
+import com.liferay.portal.search.aggregation.bucket.Range;
+import com.liferay.portal.search.aggregation.pipeline.PipelineAggregationTranslator;
+import com.liferay.portal.search.elasticsearch6.internal.aggregation.BaseFieldAggregationTranslator;
+
+import java.util.List;
+
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.PipelineAggregationBuilder;
+import org.elasticsearch.search.aggregations.bucket.range.DateRangeAggregationBuilder;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Michael C. Han
+ */
+@Component(service = RangeAggregationTranslator.class)
+public class DateRangeAggregationTranslatorImpl
+	implements DateRangeAggregationTranslator {
+
+	@Override
+	public DateRangeAggregationBuilder translate(
+		DateRangeAggregation dateRangeAggregation,
+		AggregationTranslator<AggregationBuilder> aggregationTranslator,
+		PipelineAggregationTranslator<PipelineAggregationBuilder>
+			pipelineAggregationTranslator) {
+
+		DateRangeAggregationBuilder dateRangeAggregationBuilder =
+			_baseFieldAggregationTranslator.translate(
+				baseMetricsAggregation ->
+					AggregationBuilders.dateRange(
+						baseMetricsAggregation.getName()),
+				dateRangeAggregation, aggregationTranslator,
+				pipelineAggregationTranslator);
+
+		List<Range> rangeAggregationRanges = dateRangeAggregation.getRanges();
+
+		rangeAggregationRanges.forEach(
+			rangeAggregationRange ->
+				dateRangeAggregationBuilder.addRange(
+					rangeAggregationRange.getKey(),
+					rangeAggregationRange.getFrom(),
+					rangeAggregationRange.getTo()));
+
+		return dateRangeAggregationBuilder;
+	}
+
+	private final BaseFieldAggregationTranslator
+		_baseFieldAggregationTranslator = new BaseFieldAggregationTranslator();
+
+}
